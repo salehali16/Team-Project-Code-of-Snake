ac8019334bf12a5f316d345c2d0eebbcd014ac6c
Oiseauoui <oiseua@gmail.com>
2023-10-22 20:08:50 +0300

    goodbye

--- helper.py
+++ helper.py
@@ -624,7 +624,7 @@ def main():
         'c': ['change', 'celebration', 'clean', 'close'],
         'd': ['delete'],
         'e': ['exit'],
-        'g': ['goodbye'],
+        'g': ['goodbye'], 
         'f': ['find'],
         'h': ['helper'],
         'n': ['notebook'],
